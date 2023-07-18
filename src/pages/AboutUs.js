8dbee368a86a312fbfc19f81d6f41dcf38e44f4c
Luc Schenherr <luc.schenherr@hotmail.fr>
2023-07-18 18:25:14 +0200

    Création de la page AboutUs avec le composant

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,81 +1,37 @@
 import BackgroundBanner from '../assets/img/a-propos-background.png';
-import TopArrow from '../assets/icons/top-arrow.png';
-import { useState } from 'react';
+import Collapse from '../components/Collapse';
 
 function AboutUs() {
-  const [selected, setSelected] = useState(null);
-
-  const toggle = () => {
-    if (selected === 'selected') {
-      return setSelected(null);
-    }
-    setSelected('selected');
-  };
-
   return (
     <main>
       <div className="banner">
         <img src={BackgroundBanner} alt="image background banner" />
       </div>
       <div className="colapses-container">
-        {
-          <div className="colapse-nav">
-            <div className="details-title" onClick={toggle}>
-              <h3>Fiabilité</h3>
-              <img
-                src={TopArrow}
-                alt="top-arrow"
-                className={
-                  selected === 'selected' ? 'bottom-arrow' : 'top-arrow'
-                }
-              />
-            </div>
-            <div
-              className={selected === 'selected' ? 'text display' : 'text hide'}
-            >
-              <p>
-                Les annonces postées sur Kasa garantissent une fiabilité totale.
-              </p>
-            </div>
-          </div>
-        }
-
-        <div className="colapse-nav">
-          <div className="details-title">
-            <h3>Respect</h3>
-            <img src={TopArrow} alt="top-arrow" />
-          </div>
-          <div className="text">
-            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-            comportement discriminatoire ou de perturbation du voisinage
-            entraînera une exclusion de notre plateforme.
-          </div>
-        </div>
-        <div className="colapse-nav">
-          <div className="details-title">
-            <h3>Service</h3>
-            <img src={TopArrow} alt="top-arrow" />
-          </div>
-          <div className="text">
-            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-            comportement discriminatoire ou de perturbation du voisinage
-            entraînera une exclusion de notre plateforme.
-          </div>
-        </div>
-        <div className="colapse-nav">
-          <div className="details-title">
-            <h3>Sécurité</h3>
-            <img src={TopArrow} alt="top-arrow" />
-          </div>
-          <div className="text">
-            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
-            pour les voyageurs, chaque logement correspond aux critères de
-            sécurité établis par nos services. En faisant une note aussi bien à
-            l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
-            les standards sont bien respectés. Nous organisons également des
-            ateliers sur la sécurité domestique pour nos hôtes.
-          </div>
-        </div>
+        <Collapse
+          title={'Fiabilité'}
+          description={
+            'Les annonces postées sur Kasa garantissent une fiabilité totale.'
+          }
+        />
+        <Collapse
+          title={'Respect'}
+          description={
+            'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
+          }
+        />
+        <Collapse
+          title={'Service'}
+          description={
+            'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
+          }
+        />
+        <Collapse
+          title={'Sécurité'}
+          description={
+            "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En faisant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+          }
+        />
       </div>
     </main>
   );
