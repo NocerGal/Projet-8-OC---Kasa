ab450d7faec867aa25c1b1db645d5ffb6edcd801
Luc Schenherr <luc.schenherr@hotmail.fr>
2023-08-01 16:42:53 +0200

    Page not Found terminée avec style

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,15 @@
+import { NavLink } from 'react-router-dom';
+
 function PageNotFOund() {
-  return <h1>404</h1>;
+  return (
+    <div className="error_404">
+      <p className="error">404</p>
+      <h1>Oups! La page que vous demandez n'existe pas.</h1>
+      <NavLink to="/">
+        <span>Retour à la page d'accueil</span>
+      </NavLink>
+    </div>
+  );
 }
 
 export default PageNotFOund;
